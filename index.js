ab7a79955979f7a020c2b4e01a771a90a3ccca90
Lorenzofox3 <laurent34azerty@gmail.com>
2017-03-05 12:48:20 -0500

    fix: avoid usage of this

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 export function emitter () {
 
   const listenersLists = {};
-
-  return {
+  const instance = {
     on(event, ...listeners){
       listenersLists[event] = (listenersLists[event] || []).concat(listeners);
-      return this;
+      return instance;
     },
     dispatch(event, ...args){
       const listeners = listenersLists[event] || [];
       for (let listener of listeners) {
         listener(...args);
       }
-      return this;
+      return instance;
     },
     off(event, ...listeners){
       if (!event) {
-        Object.keys(listenersLists).forEach(ev => this.off(ev));
+        Object.keys(listenersLists).forEach(ev => instance.off(ev));
       } else {
         const list = listenersLists[event] || [];
         listenersLists[event] = listeners.length ? list.filter(listener => !listeners.includes(listener)) : [];
       }
-      return this;
+      return instance;
     }
-  }
+  };
+  return instance;
 }
 
 export function proxyListener (eventMap) {
@@ -38,21 +38,19 @@ export function proxyListener (eventMap) {
       proxy[method] = function (...listeners) {
         eventListeners[ev] = eventListeners[ev].concat(listeners);
         emitter.on(ev, ...listeners);
-        return this;
+        return proxy;
       };
     }
 
     return Object.assign(proxy, {
       off(ev){
         if (!ev) {
-          Object.keys(eventListeners).forEach(eventName => this.off(eventName));
+          Object.keys(eventListeners).forEach(eventName => proxy.off(eventName));
         }
-
         if (eventListeners[ev]) {
           emitter.off(ev, ...eventListeners[ev]);
         }
-
-        return this;
+        return proxy;
       }
     });
   }
