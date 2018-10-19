418b629ae72ee6c62985b7f59bd9dec635be2d6b
Laurent Renard <laurent@Laurents-MacBook-Pro.local>
2018-10-19 09:50:47 -0400

    better packaging

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 import test from 'zora';
-import {emitter, proxyListener} from '../dist/src';
+import {emitter, proxyListener} from '../dist/bundle/module';
 
 test('register a listener to an event', t => {
     let counter = 0;
